a24ee0e5f22a88a334102c5b12d293d36c3512cd
Stefanos <sgouver@hotmail.com>
2019-10-06 19:15:01 +0100

    -images

---
+++ components/AngularjsLanding.js
@@ -0,0 +1,14 @@
+const BannerLanding = (props) => (
+    <section id="banner" className="style1">
+        <div className="inner">
+            <header className="major">
+                <h1>Angular JS</h1>
+            </header>
+            <div className="content">
+                <p>Projects based on AngularJS framework</p>
+            </div>
+        </div>
+    </section>
+)
+
+export default BannerLanding
---
+++ pages/angularJS.js
@@ -0,0 +1,54 @@
+import Head from "next/head"
+import Link from 'next/link'
+
+import Layout from '../components/Layout'
+import BannerLanding from '../components/AngularjsLanding'
+
+export default () => (
+    <Layout>
+        <Head>
+            <title>Angular JS - Projects</title>
+            <meta name="description" content="Landing Page" />
+        </Head>
+
+        <div>
+            <BannerLanding />
+
+            <div id="main">
+                <section id="one">
+                    <div className="inner">
+                        <header className="major">
+                            <h2>Inspired Thinking Group & Canopy Platform</h2>
+                        </header>
+                        <p>In our always-on world, marketing is being driven by complex data and an ever-increasing need to execute millions of unique customer interactions on a daily basis. Our powerful technology, Canopy : Deploy, and award-winning CRM team will help you cut through this multichannel mayhem and deliver hyper-personalised interactions that matter to every single customer.</p>
+                        <Link href="https://www.itg.co.uk/technology/marketing-automation/" target="_blank">Read more</Link>
+                    </div>
+                </section>
+                <section id="two" className="spotlights">
+                    <section>
+                        <Link href="/generic">
+                            <a className="image"><img src="/static/images/teamitg.png" alt="" /></a>
+                        </Link>
+                        <div className="content">
+                            <div className="inner">
+                                <header className="major">
+                                    <h3>Front-End Web Developer</h3>
+                                </header>
+                                <p>As part of the Canopy team in Inspired Thinking Group, I have the great opportunity to contribute in the development and maintenance of the transformative Canopy application.</p>
+                                <h5>Key duties:</h5>
+                                <ul>
+                                    <li>Development and maintenance of Canopy application (AngularJS).</li>
+                                    <li>New features development.</li>
+                                    <li>Design of new deployments.</li>
+                                    <li>Debugging and deploying patches based on issues reported by the clients.</li>
+                                    <li>New releases deployment with GIT (version control).</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </section>
+                </section>
+            </div>
+            
+        </div>
+    </Layout>
+)
