9c460a060e0a1c34500c59211242d690aff3a5dc
Stefanos Gkouveris <stefanosgkouveris@itg.co.uk>
2019-11-16 16:35:03 +0000

    - Completed phorcy.com page

--- pages/phorcy.js
+++ pages/phorcy.js
@@ -18,27 +18,46 @@ export default () => (
                 <section id="javascript-one">
                     <div className="inner">
                         <header className="major">
-                            <h2>About the project</h2>
+                            <h2>Abstract of the Phorcy project</h2>
                         </header>
-                        <p>A list of Wordpress based websites. The Websites were developed having in mind the SEO, responsivness and accesibility.</p>
+                        <p> 
+                            The design and operation of modern ships rely heavily on the successful management of information and
+                            amongst the various current and emerging challenges those associated with shipping safety maintain their paramount
+                            importance. A fundamental prerequisite for effective compliance with the ever-expanding domain of SOLAS regulations
+                            is the quick and reliable access to relevant information.
+                        </p>
+                        <p>
+                            In this work, an innovative information platform is developed which through its specific reference to Chapter 3 of the
+                            SOLAS Convention - Life-saving appliances and arrangements - and its applicability to Ro-Pax Ariadne (operated by
+                            Hellenic Seaways) acts as demonstrative pilot for SOLAS familiarization.
+                        </p>
                     </div>
                 </section>
                 <section id="javascript-two" className="spotlights">
                     <section>
-                        <a className="image"><img src="/static/images/political-analyst_com.png" alt="Political Analyst" /></a>
+                        <a className="image"><img src="/static/images/phorcy_com_2.png" alt="Political Analyst" /></a>
                         <div className="content">
                             <div className="inner">
                                 <header className="major">
-                                    <h3>political-analist.com</h3>
+                                    <h4>Research paper</h4>
                                 </header>
-                                <p>Developed blog/portfolio website for Mr. Ilyas Roubanis, who is a political analyst and journalist.</p>
-                                <p>To achieve the best SEO results the <strong>political-analyst.com</strong> domain was acquired.
-                                Since Mr. Roubanis is working on international basis, the <strong>.com</strong> suffix was preferred.</p>
-                                <p>The design is imitating a newspaper and trying to subvert from a typical blog site while placing the portfolio aspect as a second layer. 
-                                Typography and colours are also adjusted to help the readers to focus on the content.</p>
-
+                                <p>Stefanos Gkouveris was the lead author in the research paper: “Development of a Multimedia Interactive Platform for SOLAS Familiarization”.</p>
+                                <p>The research paper was the result of a collaboration between Dr. Artikis (Machine Learning), Dr.Tselentis (Environmental Mgmt.) & Dr.Tzannatos (Mech. Eng.).</p>
+                                <header className="major">
+                                    <h4>Platform</h4>
+                                </header>
+                                <p>To display the suggested solution mentioned in the research paper, a Front-end environment was developed using WorpPress.</p>
+                                <p>A new more elaborate future version of Phorcy.com will be builed with NextJS and is currently under construction.</p>
+                                <header className="major">
+                                    <h4>Presentations</h4>
+                                </header>
+                                <ul>
+                                    <li>In SOME 2018 conference at <a href="https://www.eef.edu.gr/en" target="_blank">Eugenides foundation</a></li>
+                                    <li>In <a href="https://www.wmu.se/" target="_blank">World Maritime University</a> M.Sc. students at <a href="https://www.unipi.gr/unipi/en/" target="_blank">University of Piraeus</a>.</li>
+                                </ul>
                                 <ul className="actions">
-                                    <li><Link href="http://political-analyst.com/"><a target="_blank" className="button">Visit</a></Link></li>
+                                    <li><Link href="http://phorcy.com/"><a target="_blank" className="button">Visit the platform</a></Link></li>
+                                    <li><Link href="https://drive.google.com/open?id=1X69T4s1KtSxnxm9zsfe8pNMp8f7-4Xdq"><a target="_blank" className="button">Research paper</a></Link></li>
                                 </ul>
                             </div>
                         </div>
